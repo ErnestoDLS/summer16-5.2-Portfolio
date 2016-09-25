8a5065611e41e38e59888dab23919eb5c157864b
Ernesto De Los Santos <netos_id@yahoo.com>
2016-09-25 15:20:54 -0500

    Able to have links on home page render but links to not able to navigate.

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ import Repos from './lib/Repos'
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-    <Route path="/about" component={About}/>
-    <Route path="/repos/:userName/:repoName" component={Repo}/>
-    <Route path="/repos" component={Repos}/>
+      <Route path="/about" component={About}/>
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
+      <Route path="/repos" component={Repos}/>
+    </Route>
   </Router>
 ), document.getElementById('app'))
